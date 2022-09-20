2c4d155751483c268736bd3dc7c564b2d96d4000
Dennis Federico <dennisfederico@gmail.com>
2022-09-21 01:00:45 +0200

    States for NPC

---
+++ states/Assets/Scripts/NpcAI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcAI : MonoBehaviour {
+
+    NavMeshAgent agent;
+    Animator anim;
+    public Transform player;
+    State currentState;
+
+    void Start() {
+        agent = this.GetComponent<NavMeshAgent>();
+        anim = this.GetComponent<Animator>();
+        currentState = new Idle(this.gameObject, agent, anim, player);
+    }
+
+    void Update() {
+        currentState = currentState.Process();
+    }
+
+}
